2f75643a688e29a5c6e75ed1f4e743006a27d53d
Ivaylo Marinkov <git@tsarstva.bg>
2015-08-27 21:14:33 +0300

    Update digest.py

--- digest.py
+++ digest.py
@@ -12,6 +12,8 @@ from datetime import datetime
 class DigestGenerator:
     
     def __init__(self, db_host, db_port, db_username, db_password, db_name, start_month, start_year, end_month, end_year):
+        self.digest_data = []
+
         self.db = MySQLdb.connect(host=db_host, user=db_username, passwd=db_password, db=db_name)
         self.cursor = self.db.cursor()
 
@@ -26,7 +28,12 @@ class DigestGenerator:
         if self.start_year > self.end_year or (self.start_year == self.end_year and self.start_month > self.end_month):
             raise ValueError('TO date %d.%d is prior to FROM date %d.%d.' % (self.end_month, self.end_year, self.start_month, self.end_month)) 
    
-    def collect_car_data(self):
+    def collect_car_data(self, digest_mode):
+        if digest_mode == "current":
+            self.end_month = self.start_month
+            self.end_year = self.start_year
+            print "Collect mode set to current month."
+
         for y in range(self.end_year - self.start_year + 1):
             year = self.start_year + y
             
@@ -39,18 +46,18 @@ class DigestGenerator:
                 if year == self.end_year and m > self.end_month:
                     break
 
-                self.receive_car_data(year, m)
+                self.digest_data.append(self.receive_car_data(year, m))
 
     def receive_car_data(self, year_index, month_index):
         print "Year %i, month %i" % (year_index, month_index)
         mindate = '%i-%s-01' % (year_index, month_index if month_index > 9 else '0' + str(month_index))
         maxdate = '%i-%s-31' % (year_index, month_index if month_index > 9 else '0' + str(month_index))                
+        digest_data_record = DigestData()
 
         self.cursor.execute(
             """select
-            make, avg(power), avg(price), avg(mileage)
+            make, power, price, mileage
             from cars where date_added between %s and %s or (date_added < %s and (active = 1 or date_deactivated < %s))
-            group by make
             order by make asc;""",
             (mindate, maxdate, mindate, maxdate))
 
@@ -61,13 +68,51 @@ class DigestGenerator:
             price = row[2]
             mileage = row[3]
 
-            print '%s %s %s %s' % (make, power, price, mileage)
+            digest_data_record.add_line(make, power, price, mileage)
+        
+        return digest_data_record
 
 
     def close_db(self):
         self.db.close()
 
-def main(): 
+class DigestData:
+
+    def __init__(self):
+        self.lines = []
+
+    def add_line(self, make, power, price, mileage):
+        line = DigestLine(make, power, price, mileage)
+        self.lines.append(line)
+
+    def get_avg_power(self):
+        valid_powers = []
+
+        for l in self.lines:
+            # Because some sellers set random numbers like 70000000
+            if l.power >= 10 and l.power <= 1200:
+                valid_powers.append(l.power)
+
+        return sum(valid_powers) / len(valid_powers)
+
+    def get_avg_price(self):
+        valid_prices = []
+
+        for l in self.lines:
+            if l.price >= 500 and l.price <= 2000000:
+                valid_prices.append(l.price)
+
+        return sum(valid_prices) / len(valid_prices)
+
+class DigestLine:
+ 
+    def __init__(self, make, power, price, mileage):
+        self.make = make
+        self.power = power
+        self.price = price
+        self.mileage = mileage
+
+def main(digest_mode): 
     config = ConfigParser.RawConfigParser()
     config.read('carwatch.ini')
     
@@ -84,8 +129,19 @@ def main():
     end_year = datetime.now().year
 
     generator = DigestGenerator(db_host, db_port, db_user, db_password, db_name, start_month, start_year, end_month, end_year)
-    generator.collect_car_data()
+    generator.collect_car_data(digest_mode)
+ 
+    for data_record in generator.digest_data:
+        print "Average horsepower: " + str(data_record.get_avg_power())
+        print "Average price: " + str(data_record.get_avg_price())
       
-if __name__ == "__main__":
-   main()
 
+if __name__ == "__main__":
+   opts = getopt.getopt(sys.argv[1:], "a:c:")
+   digest_mode = "all"
+
+   for opt in opts:
+     if opt == "-c":
+         digest_mode = "current"
+
+   main(digest_mode)
